6c6d69c4339f451e7b94106c8e4e7d28dabd22a3
Veronica <veronica.cernenchii@mail.ru>
2022-10-04 15:57:46 +0300

    ex002

--- Lesson2/ex002/Program.cs
+++ Lesson2/ex002/Program.cs
@@ -1,2 +1,14 @@
 ﻿using static System.Console;
 Clear();
+
+// f(x,y,z) = x² +7;
+Write(" Введите число х --> ");
+int x = Convert.ToInt32(ReadLine());
+Function(x);
+
+void Function (int x)
+{
+ int result = x*x+7;
+ WriteLine($" Ваш результат = [{result}] ");
+ //return result;
+}
