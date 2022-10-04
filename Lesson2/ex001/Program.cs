e2f50724c7e147f0a4365e7db9b3e613978fbaa0
Veronica <veronica.cernenchii@mail.ru>
2022-10-04 13:50:36 +0300

    modified ex001

--- Lesson2/ex001/Program.cs
+++ Lesson2/ex001/Program.cs
@@ -1,15 +1,23 @@
 ﻿using static System.Console;
+
 Clear();
+
 // f(x) = x*x +7
 
 
-Write(" Введите число x --> ");
-int x = Convert.ToInt32(ReadLine());
-function(x);
+//Write(" Введите число x --> ");
+// int x = Convert.ToInt32(ReadLine());
 
-int function (int x)
+// Write(" Введите число y --> ");
+// int y = Convert.ToInt32(ReadLine());
+
+Write(" Введите число w --> ");
+int w = Convert.ToInt32(ReadLine());
+function(w);
+
+int function(int x)
 {
- int result = x*x+7;
- System.Console.WriteLine(result);
- return result;
+    int result = x * x + 7;
+    System.Console.WriteLine(result);
+    return result;
 }
