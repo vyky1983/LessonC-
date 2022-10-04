cf5ad2e021ad88c1b157b3433fc3909c3340d9a9
Veronica <veronica.cernenchii@mail.ru>
2022-10-04 12:26:10 +0300

    Modified les3 and les2

--- Lesson3/ex001/Program.cs
+++ Lesson3/ex001/Program.cs
@@ -1,14 +1,4 @@
 ﻿using static System.Console;
 Clear();
 
-// f(x) = x *x +7; x==;
-Write("Введите число х --> ");
-int x = Convert.ToInt32(ReadLine());
 
-function(x);
-
-double function(double x)
-{
-double result = x*x+7;
-return result;
-}
