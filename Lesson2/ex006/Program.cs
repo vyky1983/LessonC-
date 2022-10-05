783c6b402a88314335a0cc93aecb4182d9b22edf
Veronica <veronica.cernenchii@mail.ru>
2022-10-05 17:56:07 +0300

    add ex006

--- Lesson2/ex006/Program.cs
+++ Lesson2/ex006/Program.cs
@@ -1,2 +1,29 @@
 ﻿using static System.Console;
+
 Clear();
+int[] array1 = new int [25];
+int[] array = new int[10];
+FillArray(array1);
+PrintArray(array1);
+
+void FillArray(int[] colection)
+{
+    int lenght = colection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        colection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Write(col[position] + ", ");
+        position++;
+    }
+}
