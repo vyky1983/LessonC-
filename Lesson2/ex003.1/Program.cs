9b9fc7e7727cb84f6c649848be1caeed59cef6aa
Veronica <veronica.cernenchii@mail.ru>
2022-10-05 15:05:36 +0300

    add new inf

--- Lesson2/ex003.1/Program.cs
+++ Lesson2/ex003.1/Program.cs
@@ -3,15 +3,17 @@ Clear();
 
 int x =125;
 int y =65;
-int z =96;
-int max= Max(x,y,z);
-WriteLine(max);
+int z =965;
+// int max= Max(x,y,z);
+// WriteLine(max);
+Max(x,y,z);
 
 int Max(int arg1,int arg2,int arg3)
 {
  int res =arg1;
  if(arg2>res)res=arg2;
  if(arg3>res)res=arg3;
+ Write($" максимальное число --> {res}");
  return res;
  }
 //Write(res);
