7e756334d5546411bbcecebb674c6bbe02cea4a3
Veronica <veronica.cernenchii@mail.ru>
2022-10-06 13:56:56 +0300

    Remove Foldre 001 on ex001 Method

--- Lesson3/ex001/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using static System.Console;
-Clear();
-
-
-
---
+++ Lesson3/ex001Method/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Console;
+Clear();
+
+//Вид 1.
+
+void Method1()
+{
+ WriteLine("--> Я автор ...");
+}  
+Method1();
+
+
+// Вид 2.
+void Method2(string smg)
+{
+ WriteLine(smg);
+}
+Method2("--> Текст сообщения");
+
+
+void Method21(string smg, int count)
+{
+ int i =0;
+ while(i<count)
+ {
+  System.Console.WriteLine(smg);
+  i++;
+ }
+}
+Method21(smg:" текст", count: 4);
+//Method2("--> Текст сообщения");
+
+
+// Вид 3.
+
