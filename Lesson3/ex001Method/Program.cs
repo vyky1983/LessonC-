62b5cbc7842ab2c2043f2a4055ab2af49f911646
Veronica <veronica.cernenchii@mail.ru>
2022-10-06 14:45:41 +0300

    Add new information in ex001Method

--- Lesson3/ex001Method/Program.cs
+++ Lesson3/ex001Method/Program.cs
@@ -1,35 +1,58 @@
-﻿using static System.Console;
-Clear();
+﻿// using static System.Console;
 
-//Вид 1.
+// Clear();
 
-void Method1()
+// //Вид 1.
+
+// void Method1()
+// {
+//     WriteLine("--> Я автор ...");
+// }
+// Method1();
+
+// // Вид 2.
+// void Method2(string smg) //принимает входные
+// {
+//     WriteLine(smg);
+// }
+// Method2("--> Текст сообщения");
+
+// void Method21(string smg, int count) // прнимает аргумены
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         System.Console.WriteLine(smg);
+//         i++;
+//     }
+// }
+// Method21(smg: " текст", count: 4);
+
+// //Method2("--> Текст сообщения");
+
+
+// // Вид 3.
+
+// int Method3()
+// {
+//     return DateTime.Now.Year;
+// }
+// Method3();
+// int year = Method3();
+// WriteLine(year);
+
+//Вид 4.
+
+string Method4(int count, string text)
 {
- WriteLine("--> Я автор ...");
-}  
-Method1();
-
-
-// Вид 2.
-void Method2(string smg)
-{
- WriteLine(smg);
+    int i = 0;
+    string result = string.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
 }
-Method2("--> Текст сообщения");
-
-
-void Method21(string smg, int count)
-{
- int i =0;
- while(i<count)
- {
-  System.Console.WriteLine(smg);
-  i++;
- }
-}
-Method21(smg:" текст", count: 4);
-//Method2("--> Текст сообщения");
-
-
-// Вид 3.
-
+string res =Method4(10, "text");
+System.Console.WriteLine(res);
