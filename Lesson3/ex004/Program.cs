ab0183b05f7a90d8c98ca9b7aa61ba0bcabf6df0
Veronica <veronica.cernenchii@mail.ru>
2022-10-06 18:39:13 +0300

    modified ex003

--- Lesson3/ex004/Program.cs
+++ Lesson3/ex004/Program.cs
@@ -1,7 +1,27 @@
 ﻿using static System.Console;
+
 Clear();
 
 /*Дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы "к" заменить большими "К", а большие "С" заменить на маленькие "с".*/
 
-string text = "Сегодня мы углубимся в тему и выделим 4 основных группы методов, а также будем использовать их на практике. И познакомимся с новыми языковыми конструкциями, в частности, циклом. ";
+string text =
+    "Сегодня мы углубимся в тему и выделим 4 основных группы методов, а также будем использовать их на практике. И познакомимся с новыми языковыми конструкциями, в частности, циклом. ";
 
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        
+            if (text[i] == oldValue) result = result + $"{newValue}";
+              
+            else result = result + $"{text[i]}";
+                
+        
+        return result;
+    }
+}
+string newText = Replace(text, ' ', '|');
+System.Console.WriteLine(newText);
