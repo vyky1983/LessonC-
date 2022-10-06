a25c526485804ccd76ff6b94fc3f53af965cefdd
Veronica <veronica.cernenchii@mail.ru>
2022-10-06 15:12:46 +0300

    add new information

---
+++ Lesson3/ex002For/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Clear();
+string res = Method4(10, "text");
+System.Console.WriteLine(res);
+
+string Method4(int count, string text)
+{
+    string result = string.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
