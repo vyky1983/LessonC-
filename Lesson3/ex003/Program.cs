f6126c0c6e966fc550a28726b8eabe4c55d7140e
Veronica <veronica.cernenchii@mail.ru>
2022-10-06 15:24:35 +0300

    Multiplication table

---
+++ Lesson3/ex003/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+Clear();
+
+for (int i = 2; i < 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        WriteLine($"{i}* {j}= {i * j}");
+    }
+    WriteLine();
+}
