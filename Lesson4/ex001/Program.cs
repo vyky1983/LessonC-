bc32b6df0d641705eb3d920242f92c1a92e6a225
Veronica <veronica.cernenchii@mail.ru>
2022-10-07 13:45:00 +0300

    Lesson3 ex001

--- Lesson4/ex001/Program.cs
+++ Lesson4/ex001/Program.cs
@@ -1,2 +1,15 @@
 ﻿using static System.Console;
 Clear();
+
+// int[] array = new int[9];
+
+// int PrintArray(int[] array)
+// {
+//  int rnd = new Random().Next(1,10);
+//  for (int i = 0; i < array.Length; i++)
+//  {
+//   int result = array[i];
+//   WriteLine(result);
+//  }
+//  return array;
+// }
