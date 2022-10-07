550b644808c34cadbfecd91a0d28bb5f5921337d
Veronica <veronica.cernenchii@mail.ru>
2022-10-07 15:13:06 +0300

    add new information ex002-1

--- Lesson4/ex2-1/Program.cs
+++ Lesson4/ex2-1/Program.cs
@@ -3,12 +3,13 @@
 Clear();
 
 int[,] matrix = new int[3, 6];
+matrix[2,2] = 9;
 
 for (int i = 0; i < matrix.GetLength(0); i++)
 {
     for (int j = 0; j < matrix.GetLength(1); j++)
     {
-        Write($"{matrix[i, j]}");
+        Write($"{matrix[i, j]} ");
     }
     System.Console.WriteLine();
 }
