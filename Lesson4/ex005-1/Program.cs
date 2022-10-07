410e265d5c1747e238a072d15f1b1b046b3b47cb
Veronica <veronica.cernenchii@mail.ru>
2022-10-07 18:31:11 +0300

    Ex005-1

--- Lesson4/ex005-1/Program.cs
+++ Lesson4/ex005-1/Program.cs
@@ -5,18 +5,33 @@ Clear();
 // int[,] arra = new int[5, 6];
 // FillArray(arra);
 
-int[,] array1 = new int[4,4];
-FillArray(array1);
+// int[,] array1 = new int[4,4];
+// FillArray(array1);
 
-void FillArray(int[,] array)
+// void FillArray(int[,] array)
+// {
+//     for (int row = 0; row < array.GetLength(0); row++)
+//     {
+//         for (int colum = 0; colum < array.GetLength(0); colum++)
+//         {
+//             array[row, colum] = new Random().Next(1, 10);
+//             Write($"{array[row, colum]} * ");
+//         }
+//         System.Console.WriteLine();
+//     }
+// }
+
+int[,] arrdo = new int [5,6];
+FillArray(arrdo);
+
+void FillArray(int[,] arrdouble){
+for (int i = 0; i < arrdouble.GetLength(0); i++)
 {
-    for (int row = 0; row < array.GetLength(0); row++)
-    {
-        for (int colum = 0; colum < array.GetLength(0); colum++)
-        {
-            array[row, colum] = new Random().Next(1, 10);
-            Write($"{array[row, colum]} * ");
-        }
-        System.Console.WriteLine();
-    }
+ for (int j = 0; j < arrdouble.GetLength(1); j++)
+ {
+  arrdouble[i,j]=new Random().Next(1,10);
+  Write($"{arrdouble[i,j]} ");
+ }
+ WriteLine();
+}
 }
