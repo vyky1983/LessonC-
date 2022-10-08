48760c884602fad8b47159b720e8c8faf54f41df
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-08 15:48:26 +0300

    Add ex003, ex006

---
+++ Lesson5/ex003/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Clear();
+int[] array = new int[5] { 1, 2, 3, 4, 5 };
+ReverseArray(array);
+
+void ReverseArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        // int rnd = new Random().Next(1, 10);
+        // arr[i] = rnd;
+        Write($"{arr[i]} ");
+    //     System.Console.WriteLine();
+     }
+}
---
+++ Lesson5/ex006/Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+
+/**/
+
+ double a = 2.36;
+System.Console.WriteLine(a.GetType().Name );
+
+var data =new int[]{1,2,3,4,};
+System.Console.WriteLine(data.GetType().Name);
