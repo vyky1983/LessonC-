911ac75bb3cfcf36e4bd82a4089063260d947f6c
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-08 13:35:46 +0300

    Add new Folder Les5 уч001буч002

--- Lesson5/ex001/Program.cs
+++ Lesson5/ex001/Program.cs
@@ -1,2 +1,15 @@
 ﻿using static System.Console;
 Clear();
+
+int[] array =new int[10];
+PrintArray(array);
+
+ void PrintArray(int[] arr)
+ {
+  for(int i=0; i<arr.Length; i++)
+  {
+   int rnd =new Random().Next(1,10);
+   arr[i] =rnd;
+   Write($"{arr[i]} ");
+  }
+ }
--- Lesson5/ex002/Program.cs
+++ Lesson5/ex002/Program.cs
@@ -1,2 +1,20 @@
 ﻿using static System.Console;
+
 Clear();
+
+int[,] array = new int[6, 6];
+PrintArray(array);
+
+void PrintArray(int[,] arr)
+{
+    for (int sow = 0; sow < arr.GetLength(0); sow++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            int rnd = new Random().Next(1, 10);
+            arr[sow, j] = rnd;
+            Write($"{arr[sow, j]} ");
+        }
+        WriteLine();
+    }
+}
