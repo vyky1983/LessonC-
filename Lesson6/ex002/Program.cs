03550e8faac2953f563a1d4d7e6353259b70ae09
vyky1983 <veronica.cernenchii@mail.ru>
2022-10-10 14:22:37 +0300

    add new information

--- Lesson6/ex002/Program.cs
+++ Lesson6/ex002/Program.cs
@@ -1,2 +1,15 @@
 ﻿using static System.Console;
 Clear();
+
+// using System.Linq;
+
+string text="(1,2)(2,3)(4,6)(6,7)".Replace("(","");
+text="(1,2)(2,3)(4,6)(6,7)".Replace(")","");
+WriteLine(text);
+
+var data =text.Split(" ").ToArray();
+       // .;
+for (int i = 0; i < data.Length; i++)
+{
+ WriteLine(data[i]);
+}
